90435ccdbd08ef533621706aec6bc3c5fe576153
skolopendrochka <katya.chech@yandex.ru>
2025-07-06 16:05:57 +0300

    Create service-worker.js

---
+++ dz12/service-worker.js
@@ -0,0 +1,60 @@
+const CACHE_NAME = 'secure-pwa-v1';
+const OFFLINE_URL = 'offline.html';
+const ASSETS_TO_CACHE = [
+  '/',
+  '/index.html',
+  '/login.html',
+  '/styles/main.css',
+  '/app.js',
+  '/auth.js'
+];
+
+// Install event
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        return cache.addAll(ASSETS_TO_CACHE);
+      })
+  );
+});
+
+// Fetch event
+self.addEventListener('fetch', (event) => {
+  if (event.request.mode === 'navigate') {
+    event.respondWith(
+      fetch(event.request)
+        .catch(() => {
+          return caches.match(OFFLINE_URL);
+        })
+    );
+  } else {
+    event.respondWith(
+      caches.match(event.request)
+        .then((response) => {
+          return response || fetch(event.request);
+        })
+        .catch(() => {
+          if (event.request.headers.get('accept').includes('text/html')) {
+            return caches.match(OFFLINE_URL);
+          }
+        })
+    );
+  }
+});
+
+// Activate event
+self.addEventListener('activate', (event) => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
